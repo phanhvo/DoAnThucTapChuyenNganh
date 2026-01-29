1b8840d48f21b0e239ac5bb4dabb3dd1e4458d32
vophuonganh <phuonganhdth123@gmail.com>
2026-01-29 15:07:18 +0700

    them file bao cao do an

--- hotel-be/routes/khachhangRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const khachhangController = require("../controllers/khachhangController");
-router.get("/", khachhangController.getAll);
-router.post("/", khachhangController.add);
-router.post("/dangky", khachhangController.register);
-router.put("/:MaKH", khachhangController.update);
-router.get("/:MaKH", khachhangController.getById);
-module.exports = router;
